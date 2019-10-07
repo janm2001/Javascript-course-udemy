5c374f6965627d9386d19019e73e3b0f937844f1
janm2001 <30762794+janm2001@users.noreply.github.com>
2019-10-07 18:41:08 +0200

    Add files via upload
    New functionalities to the game like hold and new game and to add new clases when we have a winner etc.

--- app.js
+++ app.js
@@ -8,22 +8,58 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 // use innerHtml is your want to change the elements and textContent if you only want to write text 
 */
-
-var scores = [0,0];
-var roundScore = 0;
-var activePlayer = 0;
+var scores,activePlayer,roundScore;
+init();
 
 var dice = Math.floor((Math.random() *6)) +1;
 console.log(dice);
 
 
 //HIDES THE DICE IMAGE ELEMENT UNTIL WE START TO PLAY THE GAME
-document.querySelector(".dice").style.display = "none";
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        //same as if(activeplayer === 0) activeplayer = 1;else...
+        //setting roundScore to 0 so we could reset the round because the round is over
+        roundScore = 0;
+        //displaying contents to the user
+        document.getElementById("current-0").textContent = "0";
+        document.getElementById("current-1").textContent = "0";
+        //Toogle class active so if there is class active it would be removed if there is not it would be added
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        //document.querySelector(".player-0-panel").classList.add("active");
+       // document.querySelector(".player-1-panel").classList.remove("active");
+       //Hide the dice so the next player can play and see his dice
+       document.querySelector(".dice").style.display="none";
+}
+
+function init(){
+    scores = [0,0];
+    activePlayer=0;
+    roundScore=0;
+
+
+    document.querySelector(".dice").style.display = "none";
 
 document.getElementById("score-0").textContent="0";
 document.getElementById("score-1").textContent="0";
 document.getElementById("current-0").textContent="0";
 document.getElementById("current-1").textContent="0";
+
+document.querySelector("#name-0").textContent="Player 1";
+document.querySelector("#name-1").textContent="Player 2";
+
+document.querySelector(".player-0-panel").classList.remove("winner");
+document.querySelector(".player-1-panel").classList.remove("winner");
+document.querySelector(".player-0-panel").classList.remove("active");
+document.querySelector(".player-1-panel").classList.remove("active");
+
+document.querySelector(".player-0-panel").classList.add("active");
+document.querySelector(".player-1-panel").classList.remove("active");
+}
 
 
 
@@ -45,23 +81,7 @@ document.querySelector(".btn-roll").addEventListener("click",function(){
 
 
     }else{
-        //Next player
-        //Better was to check if players 1 or player 2 is active is also cleaner and faster
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        //same as if(activeplayer === 0) activeplayer = 1;else...
-        //setting roundScore to 0 so we could reset the round because the round is over
-        roundScore = 0;
-        //displaying contents to the user
-        document.getElementById("current-0").textContent = "0";
-        document.getElementById("current-1").textContent = "0";
-        //Toogle class active so if there is class active it would be removed if there is not it would be added
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-
-        //document.querySelector(".player-0-panel").classList.add("active");
-       // document.querySelector(".player-1-panel").classList.remove("active");
-       //Hide the dice so the next player can play and see his dice
-       document.querySelector(".dice").style.display="none";
+        nextPlayer();
             
         
     }
@@ -69,6 +89,32 @@ document.querySelector(".btn-roll").addEventListener("click",function(){
 //or we could use anomyous function by putting a function in second argument
 
 
+//DRY CODE is a code where we do not repeat some code instead we make it cleaner and faster
+
+document.querySelector(".btn-hold").addEventListener("click", function(){
+    //Add current score to the GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    //Update the user interface
+    document.querySelector("#score-"+activePlayer).textContent= scores[activePlayer];
+    
+
+    
+
+    //Check if player won the game
+    if(scores[activePlayer]>=100){
+        document.querySelector("#name-"+activePlayer).textContent="Winner!";
+        document.querySelector(".dice").style.display="none";//Hide the rolling dice picture
+        document.querySelector("player-"+activePlayer+"-panel").classList.add("winner");
+        document.querySelector("player-"+activePlayer+"-panel").classList.remove("active");
+    }else{
+        //Change a player turn if there is no winner
+    nextPlayer();
+    }
+    
+});
+
+document.querySelector(".btn-new").addEventListener("click",init);
 
 
 
